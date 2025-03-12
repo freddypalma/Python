7589aa0898caf0951364b6db43d59376e8743a17
freddypalma <freddypalma@gmail.com>
2025-03-12 00:28:31 +0000

    Trabajamos con streamlie

---
+++ app.py
@@ -0,0 +1,3 @@
+import streamlit as st # importamos libreria streamlit
+st.write("Hello World") # Imprimie en pantalla
+# llamamos el programa: streamlit run app.py
---
+++ pruebas_streamlite.py
@@ -0,0 +1,5 @@
+# crear el entorno virtual  python -m venv .venv
+# se crea la carpeta .venv y dentro de esta carpeta esta la carpeta bin donde esta el activador del ambiente virtual
+# activamos el hambiente virtual:  source .venv/bin/activate
+# instalamos la libreria con :  pip install streamlit
+# desactivamos el ambiente  : deactivate
